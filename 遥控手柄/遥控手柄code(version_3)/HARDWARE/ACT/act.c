0cdfcbd2641d01a99953e2412c66ab2d3e0748ae
Zhiqiang Ai <42333433+azq133900@users.noreply.github.com>
2019-08-29 22:47:38 +0800

    3rd commit
    开始加入了遥控器对于丝杆电机以及两个阀这些执行器的控制功能，暂只有显示部分，没有发送单元内容，待补

---
+++ 遥控手柄/遥控手柄code(version_3)/HARDWARE/ACT/act.c
@@ -0,0 +1,346 @@
+#include "act.h"
+#include "delay.h"
+#include "sys.h"
+#include "lcd.h"
+#include "gui.h"
+#include "adc.h"
+#include "key.h"
+#include "usart.h"
+
+#include "stm32f10x_gpio.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "String.h"
+#define backcolor BLACK
+#define OPModeAnalogmax 999.f
+#define CPModeAnalogmax 200.f
+#define OPModeAnalogmin 0.f
+#define CPModeAnalogmin 50.f
+#define pointerlen 22
+
+#define sampleperiod    20   //unit:ms
+#define lcdrefreshperiod 100 //unit:ms
+u8 mode=2;
+
+u16 Analogvalue1=0;
+u16 Analogvalue2=0;
+int Analogvalue3=0;
+int Analogvalue4=0;
+u8  An1Len=0;
+u8  An2Len=0;
+u8  preAn1Len=0;
+u8  preAn2Len=0;
+short int xpointer=0;
+short int ypointer=0;
+
+u16 value3base=2050;
+u16 value4base=2050;
+int value3temp=2050;
+int value4temp=2050;
+
+u8 Valve1=0;
+u8 Valve2=0;
+
+void togglemode()
+{
+	if(!mode){mode=1;}
+	else if(mode==1){mode=2;}
+	else if(mode==2){mode=3;}
+	else{mode=0;}
+	display_init();
+}
+
+void trans_analog_value(u8 sw)
+{
+	if(mode==1)
+	{
+	Analogvalue1=(u16)(AD_Value[1]*(CPModeAnalogmax-CPModeAnalogmin)/4095)+CPModeAnalogmin;
+	Analogvalue2=(u16)(AD_Value[0]*(CPModeAnalogmax-CPModeAnalogmin)/4095)+CPModeAnalogmin;
+	}
+	else
+	{
+	Analogvalue1=(u16)(AD_Value[1]*(OPModeAnalogmax-OPModeAnalogmin)/4095)+OPModeAnalogmin;
+	Analogvalue2=(u16)(AD_Value[0]*(OPModeAnalogmax-OPModeAnalogmin)/4095)+OPModeAnalogmin;
+	}
+	
+//不带修正版本
+//	Analogvalue4=AD_Value[2];
+//	Analogvalue3=AD_Value[3];
+//	if(Analogvalue4>2143){Analogvalue4=(Analogvalue4-2113)*0.504036f;}
+//	else if((Analogvalue4<2083)){Analogvalue4=(Analogvalue4-2113)*0.4727875f;}
+//	else{Analogvalue4=0;}
+//	if(Analogvalue3>2023){Analogvalue3=(1993-Analogvalue3)*0.47526f;}
+//	else if(Analogvalue3<1963){Analogvalue3=(1993-Analogvalue3)*0.50125f;}
+//	else{Analogvalue3=0;}
+//	xpointer=(2048-AD_Value[3])*pointerlen/2048;
+//	ypointer=(2048-AD_Value[2])*pointerlen/2048;
+////带修正版本
+	value4temp=AD_Value[2]-value4base;
+	value3temp=AD_Value[3]-value3base;
+	if(value4temp>50){Analogvalue4=value4temp*999.f/(4096-value4base);}
+	else if((value4temp<-50)){Analogvalue4=value4temp*999.f/value4base;}
+	else{Analogvalue4=0;}
+	if(value3temp>50){Analogvalue3=(-value3temp*999.f/(4096-value3base));}
+	else if(value3temp<-50){Analogvalue3=-value3temp*999.f/value3base;}
+	else{Analogvalue3=0;}
+	xpointer=Analogvalue3*pointerlen/1000;
+	ypointer=-Analogvalue4*pointerlen/1000;
+	
+	if(sw)
+	{      
+	preAn1Len=An1Len;
+	preAn2Len=An2Len;
+	An1Len=(u8)(AD_Value[1]*128.f/4095);
+	An2Len=(u8)(AD_Value[0]*128.f/4095);
+	}
+}
+void display_init(void)
+{
+	LCD_Clear(backcolor);    //清屏
+	Show_Str(0,0,WHITE,backcolor,"MODE:",12,1);
+	
+  LCD_DrawLine(0,63,128,63);
+	LCD_DrawLine(0,64,128,64);
+	Show_Str(0,65,WHITE,backcolor,"Last Sent:",12,1);
+	 LCD_DrawLine(0,97,128,97);
+	 LCD_DrawLine(0,98,128,98);
+	Show_Str(0,99,WHITE,backcolor,"Feedback:",12,1);
+	//mode=2;
+	refresh_mode();
+
+}
+
+
+
+
+void refresh_mode(void)
+{
+	LCD_Fill(38,2,128,12,backcolor);
+	LCD_Fill(0,10,75,24,backcolor);
+	LCD_Fill(0,36,75,48,backcolor);
+	if(mode==1)
+	{
+		Show_Str(38,0,YELLOW,backcolor,"CLOSE LOOP Ctr",12,1);
+		Show_Str(0,12,BLUE,backcolor,"LenToCover1:",12,1);
+		Show_Str(0,36,BLUE,backcolor,"LenToCover2:",12,1);
+		Show_Str(105,12,WHITE,backcolor,"mm",12,1);
+		Show_Str(105,36,WHITE,backcolor,"mm",12,1);
+		An2Len=An1Len=0;
+	}
+	else if(mode==0)
+	{
+		Show_Str(38,0,GREEN,backcolor,"OPEN LOOP Ctr",12,1);
+		Show_Str(0,12,GBLUE,backcolor,"1Step Power1:",12,1);
+		Show_Str(0,36,GBLUE,backcolor,"1Step Power2:",12,1);
+		Show_Str(105,12,WHITE,backcolor,"P*S",12,1);
+		Show_Str(105,36,WHITE,backcolor,"P*S",12,1);
+		An2Len=An1Len=0;
+	}
+	else if(mode==2)
+	{
+	 Show_Str(38,0,BRED,backcolor,"FREE MOTOR Ctr",12,1);
+	 Show_Str(0,12,YELLOW,backcolor,"X-Direction:",12,1);
+	 Show_Str(32,25,WHITE,backcolor,"mm/s",12,1);
+	 Show_Str(0,36,RED,backcolor,"Y-Direction:",12,1);
+	 Show_Str(32,49,WHITE,backcolor,"mm/s",12,1);
+	 LCD_Fill(72,13,128,62,GRED);
+	 LCD_Fill(74,15,126,60,backcolor);
+   value4base=AD_Value[2];
+	 value3base=AD_Value[3];
+	}
+	else if(mode==3)
+	{
+		Show_Str(38,0,GBLUE,backcolor,"ACTUATOR Ctr",12,1);
+		Show_Str(0,12,GREEN,backcolor,"Manual Ctr:",12,1);
+		Show_Str(0,24,YELLOW,backcolor,"Auto Ctr  :",12,1);
+		Show_Str(105,12,WHITE,backcolor,"%",12,1);
+		Show_Str(105,24,WHITE,backcolor,"%",12,1);
+		Show_Str(0,36,RED,backcolor,"VALVE1    :",12,1);
+		Show_Str(0,48,BLUE,backcolor,"VALVE2    :",12,1);
+	}
+	LCD_DrawLine(0,11,128,11);
+	LCD_DrawLine(0,12,128,12);
+}
+
+void refresh_analog_value(void)
+{
+	trans_analog_value(1);
+if(mode==1||mode==0)
+{
+	LCD_Fill(76,13,104,24,backcolor);
+	LCD_Fill(76,36,104,48,backcolor);
+	LCD_ShowNum(85,12,Analogvalue1,3,12);
+	LCD_ShowNum(85,36,Analogvalue2,3,12);
+	if(An1Len>preAn1Len)
+	{
+		LCD_Fill(preAn1Len,25,An1Len,35,YELLOW);
+	}
+	else if(An1Len<preAn1Len)
+	{
+		LCD_Fill(An1Len,25,preAn1Len,35,backcolor);
+	}
+	if(An2Len>preAn2Len)
+	{
+		LCD_Fill(preAn2Len,49,An2Len,61,GREEN);
+	}
+	else if(An2Len<preAn2Len)
+	{
+		LCD_Fill(An2Len,49,preAn2Len,61,backcolor);
+	}
+}
+else if(mode==2)
+{
+	LCD_Fill(0,25,31,37,backcolor);
+	LCD_Fill(0,49,31,61,backcolor);
+	LCD_Fill(74,15,126,60,backcolor);
+	LCD_DrawLine(100,37,(u8)(100+xpointer),(u8)(37+ypointer));
+	if(Analogvalue3>=0)
+	{Show_Str(0,25,GREEN,backcolor,"+",12,1);
+	LCD_ShowNum(10,25,(u16)Analogvalue3,3,12);}
+	else{Show_Str(0,25,BLUE,backcolor,"-",12,1);
+	LCD_ShowNum(10,25,(u16)(-Analogvalue3),3,12);}
+	
+	if(Analogvalue4>=0)
+	{Show_Str(0,49,GREEN,backcolor,"+",12,1);
+	LCD_ShowNum(10,49,(u16)Analogvalue4,3,12);}
+	else{Show_Str(0,49,BLUE,backcolor,"-",12,1);
+	LCD_ShowNum(10,49,(u16)(-Analogvalue4),3,12);}
+}
+else if(mode==3)
+{
+	LCD_Fill(69,13,104,60,backcolor);
+	//LCD_Fill(69,25,104,36,backcolor);
+	if(Analogvalue3>=0)
+	{Show_Str(70,12,GREEN,backcolor,"+",12,1);
+	LCD_ShowNum(85,12,(u16)(Analogvalue3/10),3,12);}
+	else{Show_Str(70,12,BLUE,backcolor,"-",12,1);
+	LCD_ShowNum(85,12,(u16)(-Analogvalue3/10),3,12);}
+	LCD_ShowNum(85,24,Analogvalue1/10,3,12);
+	
+	LCD_ShowNum(85,36,Valve1,3,12);
+	LCD_ShowNum(85,48,Valve2,3,12);
+}
+
+}
+
+void writelastsent (u8 *str)
+{
+	LCD_Fill(0,78,128,94,backcolor);
+	Show_Str(0,78,MAGENTA,backcolor,str,16,1);
+}
+void writefeedback (u8 *str)
+{
+	LCD_Fill(0,112,128,128,backcolor);
+	Show_Str(0,112,GBLUE,backcolor,str,16,1);
+}
+
+void work(void)
+{
+	u8 count=lcdrefreshperiod/sampleperiod;
+	u8 tempstr[10];
+	display_init();
+	while(1)
+	{
+		count=lcdrefreshperiod/sampleperiod;
+	  while(count--)
+	  {
+			delay_ms(sampleperiod);
+	    read_key();
+			if(key_value)
+			{
+			if(pup)
+			{trans_analog_value(0);
+				if(!mode)
+        {printf("A3%d%d",(Analogvalue1/100),(Analogvalue2/10));
+				 sprintf(tempstr,"%s%d%s%d","UP P:",(Analogvalue1/100)*100," T:",(Analogvalue2/10));
+				 writelastsent(tempstr);
+				}
+				else if(mode==1){
+					if(Analogvalue1>99){printf("C0%d",Analogvalue1);}
+					else{printf("C00%d",Analogvalue1);}
+					sprintf(tempstr,"%s%d%s","UP: ",Analogvalue1,"mm");
+				  writelastsent(tempstr);
+				}
+			}
+			
+			if(pdown)
+			{trans_analog_value(0);
+				if(!mode)
+        {printf("A4%d%d",(Analogvalue1/100),(Analogvalue2/10));
+				 sprintf(tempstr,"%s%d%s%d","DOWN P:",(Analogvalue1/100)*100," T:",(Analogvalue2/10));
+				 writelastsent(tempstr);
+				}
+				else if(mode==1){
+				if(Analogvalue1>99){printf("C1%d",Analogvalue1);}
+				else{printf("C10%d",Analogvalue1);}
+			  sprintf(tempstr,"%s%d%s","DOWN: ",Analogvalue1,"mm");
+				writelastsent(tempstr);
+				}
+			}
+			
+			
+			if(pright)
+			{trans_analog_value(0);
+				if(!mode)
+        {printf("B3%d%d",(Analogvalue1/100),(Analogvalue2/10));
+				 sprintf(tempstr,"%s%d%s%d","RIGHT P:",(Analogvalue1/100)*100," T:",(Analogvalue2/10));
+				 writelastsent(tempstr);
+				}
+			}
+			
+			if(pleft)
+			{trans_analog_value(0);
+				if(!mode)
+        {printf("B4%d%d",(Analogvalue1/100),(Analogvalue2/10));
+				 sprintf(tempstr,"%s%d%s%d","LEFT P:",(Analogvalue1/100)*100," T:",(Analogvalue2/10));
+				 writelastsent(tempstr);
+				}
+				
+			}
+			
+			
+			
+			if(kb){printf("A2000C2000");writelastsent("STOP ALL");}
+			if(pa){pa=0;togglemode();printf("A2000C2000");writelastsent("STOP ALL");}
+			}
+			if(mode==2)
+			{   
+				  refresh_analog_value();
+				/*
+				  //The part below is for version 1 which can only control Motor A but Motor B
+					if(Analogvalue4>99){printf("A0%d",Analogvalue4);sprintf(tempstr,"%s%d%s","UP: ",Analogvalue4," P");writelastsent(tempstr);}
+					else if(Analogvalue4>9){printf("A00%d",Analogvalue4);sprintf(tempstr,"%s%d%s","UP: ",Analogvalue4," P");writelastsent(tempstr);}
+					else if(Analogvalue4>0){printf("A000%d",Analogvalue4);sprintf(tempstr,"%s%d%s","UP: ",Analogvalue4," P");writelastsent(tempstr);}
+					else if(Analogvalue4==0){printf("A2000");writelastsent("STOPA");}
+					else if(Analogvalue4>-10){printf("A100%d",(u16)(-Analogvalue4));sprintf(tempstr,"%s%d%s","DOWN: ",Analogvalue4," P");writelastsent(tempstr);}
+					else if(Analogvalue4>-100){printf("A10%d",(u16)(-Analogvalue4));sprintf(tempstr,"%s%d%s","DOWN: ",Analogvalue4," P");writelastsent(tempstr);}
+					else {printf("A1%d",(u16)(-Analogvalue4));sprintf(tempstr,"%s%d%s","DOWN: ",Analogvalue4," P");writelastsent(tempstr);}
+				*/
+					if(Analogvalue4>99){printf("A0%d",Analogvalue4);}
+					else if(Analogvalue4>9){printf("A00%d",Analogvalue4);}
+					else if(Analogvalue4>0){printf("A000%d",Analogvalue4);}
+					else if(Analogvalue4==0){printf("A2000");}
+					else if(Analogvalue4>-10){printf("A100%d",(u16)(-Analogvalue4));}
+					else if(Analogvalue4>-100){printf("A10%d",(u16)(-Analogvalue4));}
+					else {printf("A1%d",(u16)(-Analogvalue4));}
+				
+					if(Analogvalue3>99){printf("B0%d",Analogvalue3);}
+					else if(Analogvalue3>9){printf("B00%d",Analogvalue3);}
+					else if(Analogvalue3>0){printf("B000%d",Analogvalue3);}
+					else if(Analogvalue3==0){printf("B2000");}
+					else if(Analogvalue3>-10){printf("B100%d",(u16)(-Analogvalue3));}
+					else if(Analogvalue3>-100){printf("B10%d",(u16)(-Analogvalue3));}
+					else {printf("B1%d",(u16)(-Analogvalue3));}
+					
+					
+					
+					sprintf(tempstr,"%s%d%s%d","V.:",Analogvalue4,"H.:",Analogvalue3);writelastsent(tempstr);
+					delay_ms(lcdrefreshperiod);
+			}
+	  }
+		refresh_analog_value();
+  } 
+	
+}
+
+
---
+++ 遥控手柄/遥控手柄code(version_3)/HARDWARE/ADC/adc.c
@@ -0,0 +1,98 @@
+#include "adc.h"
+#include "delay.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_adc.h"
+#include "stm32f10x_tim.h"
+
+u16 X_value;
+u16 Y_value;
+u16 A1_value;
+u16 A2_value;
+u16 AD_Value[4];
+void  Adc_Init(void)
+{ 	
+	ADC_InitTypeDef ADC_InitStructure; 
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA |RCC_APB2Periph_ADC1	, ENABLE );	 
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+	RCC_ADCCLKConfig(RCC_PCLK2_Div6);   
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_2|GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;		
+	GPIO_Init(GPIOA, &GPIO_InitStructure);	
+	
+	ADC_DeInit(ADC1); 
+	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;	
+	ADC_InitStructure.ADC_ScanConvMode = ENABLE;	
+	ADC_InitStructure.ADC_ContinuousConvMode =ENABLE;	
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_NbrOfChannel = 4;	
+	ADC_Init(ADC1, &ADC_InitStructure);	 
+
+  ADC_RegularChannelConfig(ADC1, 	ADC_Channel_0, 1, ADC_SampleTime_239Cycles5 );
+	ADC_RegularChannelConfig(ADC1, 	ADC_Channel_1, 2, ADC_SampleTime_239Cycles5 );	
+  ADC_RegularChannelConfig(ADC1, 	ADC_Channel_2, 3, ADC_SampleTime_239Cycles5 );
+	ADC_RegularChannelConfig(ADC1, 	ADC_Channel_3, 4, ADC_SampleTime_239Cycles5 );	
+
+  ADC_DMACmd(ADC1,ENABLE);
+	
+	ADC_Cmd(ADC1, ENABLE);
+	ADC_ResetCalibration(ADC1);	 
+	while(ADC_GetResetCalibrationStatus(ADC1));	
+	ADC_StartCalibration(ADC1);	 
+	while(ADC_GetCalibrationStatus(ADC1));	
+
+}			
+ 
+void Dma_Init(void)
+{
+	
+DMA_InitTypeDef            DMA_InitStructure;	
+RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE); 
+DMA_DeInit( DMA1_Channel1 );   
+DMA_InitStructure.DMA_PeripheralBaseAddr = (u32)&ADC1->DR; 
+DMA_InitStructure.DMA_MemoryBaseAddr = (u32)&AD_Value;  
+DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+DMA_InitStructure.DMA_BufferSize = 4; 
+DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable; 
+DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;  
+DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord; 
+DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;  
+DMA_InitStructure.DMA_Mode = DMA_Mode_Circular; 
+DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;  
+DMA_Init( DMA1_Channel1, &DMA_InitStructure );
+
+}
+
+void Analog_Sample_Init(void)
+{
+	Adc_Init();
+	Dma_Init();
+}
+void Analog_Sample_Switch(u8 sw)
+{
+	if(sw)
+	{
+		ADC_SoftwareStartConvCmd(ADC1,ENABLE);
+		DMA_Cmd(DMA1_Channel1,ENABLE);
+	}
+	else
+	{
+		ADC_SoftwareStartConvCmd(ADC1,DISABLE);
+		DMA_Cmd(DMA1_Channel1,DISABLE);
+	}
+}
+
+void Get_Analog_Value(void)   
+{
+	A1_value=AD_Value[0];
+	A2_value=AD_Value[1];
+	X_value=AD_Value[2];
+	Y_value=AD_Value[3];
+}
+
+
+
+
